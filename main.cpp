2801a8d5cadff31d59b2e04007605dc83c14634a
bell-404err <bohdan.dev404@gmail.com>
2025-12-01 13:59:48 +0200

    missing-number

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "two-sum.h"
+#include "missing-number.h"
 
 using std::cout;
 using std::vector;
@@ -18,31 +19,40 @@ void printVector(const vector<int>& vec) {
 int main() {
 
   // Two-sum: return indexes of two elements that equal to target ------------------------------------------------------
-  const vector<int> firstCase = { 2, 7, 11, 15 }; // target: 9
-  const vector<int> secondCase = { 3, 2, 4 }; // target: 6
-  const vector<int> thirdCase = { 3, 3 }; // target: 6
-
-  const vector<int> resultFirstTarget = twoSum(firstCase, 9);
-  const vector<int> resultSecondTarget = twoSum(secondCase, 6);
-  const vector<int> resultThirdTarget = twoSum(thirdCase, 6);
-
-  if (!resultFirstTarget.empty()) {
-    cout << "Output must be: [0, 1]\n";
-    printVector(resultFirstTarget);
-  }
-
-  if (!resultSecondTarget.empty()) {
-    cout << "Output must be: [1, 2]\n";
-    printVector(resultSecondTarget);
-  }
-
-  if (!resultThirdTarget.empty()) {
-    cout << "Output must be: [0, 1]\n";
-    printVector(resultThirdTarget);
-  }
+  // const vector<int> firstCase = { 2, 7, 11, 15 }; // target: 9
+  // const vector<int> secondCase = { 3, 2, 4 }; // target: 6
+  // const vector<int> thirdCase = { 3, 3 }; // target: 6
+  //
+  // const vector<int> resultFirstTarget = twoSum(firstCase, 9);
+  // const vector<int> resultSecondTarget = twoSum(secondCase, 6);
+  // const vector<int> resultThirdTarget = twoSum(thirdCase, 6);
+  //
+  // if (!resultFirstTarget.empty()) {
+  //   cout << "Output must be: [0, 1]\n";
+  //   printVector(resultFirstTarget);
+  // }
+  //
+  // if (!resultSecondTarget.empty()) {
+  //   cout << "Output must be: [1, 2]\n";
+  //   printVector(resultSecondTarget);
+  // }
+  //
+  // if (!resultThirdTarget.empty()) {
+  //   cout << "Output must be: [0, 1]\n";
+  //   printVector(resultThirdTarget);
+  // }
   // 01.12.2025 --------------------------------------------------------------------------------------------------------
   // -------------------------------------------------------------------------------------------------------------------
 
+  // Missing number ----------------------------------------------------------------------------------------------------
+  const vector<int> firstCase = { 3, 0, 1 };
+  const vector<int> secondCase = { 0, 1 };
+  const vector<int> thirdCase = { 9, 6, 4, 2, 3, 5, 7, 0, 1 };
+  missingNumber(firstCase);
+  missingNumber(secondCase);
+  missingNumber(thirdCase);
+  // 01.12.2025 --------------------------------------------------------------------------------------------------------
+  //--------------------------------------------------------------------------------------------------------------------
   return 0;
 }
 
